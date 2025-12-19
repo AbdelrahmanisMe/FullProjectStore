870969fa1e853c03ed0725a46efb466c9fb69c93
AHMED MOHAMED <74484905+bebo2114@users.noreply.github.com>
2025-12-19 16:41:10 +0200

    Refactor Swiper initialization with autoplay option

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-
-var swiper = new Swiper(".mySwiper", {
-      spaceBetween: 30,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
+const swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
     
 let products;
